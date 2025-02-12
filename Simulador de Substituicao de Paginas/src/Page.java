7d0e0ec824e36ce390ead0c67fd4d932b762e7f9
EmilyCarla <125914481+EmilyCarla@users.noreply.github.com>
2025-02-12 20:16:12 -0300

    Add files via upload

---
+++ Simulador de Substituicao de Paginas/src/Page.java
@@ -0,0 +1,13 @@
+public class Page {
+    int N, I, D, R, M, T;
+
+    public Page(int N, int I, int D, int R, int M, int T) {
+        this.N = N;
+        this.I = I;
+        this.D = D;
+        this.R = R;
+        this.M = M;
+        this.T = T;
+    }
+}
+
